a34d7cdee2d81132fb6a6ba807ccfa7b4fb68ac4
Pedro Rodrigues <miguelms19@gmail.com>
2017-12-26 23:22:17 +0000

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,12 @@ import pytz # timezone
 import requests
 import os
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def home_page():
-	return render_template('index.html')
+	return 'This is the home page'
 
-@app.route('/new_customer')
-def new_customer_page():
-	return render_template('new_customer.html')
-
-
-
-
-app.run(host=os.getenv('IP', '0.0.0.0'), port = int(os.getenv('PORT', 8080)))
 
 if __name__ == '__main__':
 	app.run(debug=False)
