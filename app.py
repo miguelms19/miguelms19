6327b86174f2b4f19133c3719b3dd6c46ed9c3a7
Pedro Rodrigues <miguelms19@gmail.com>
2017-12-27 00:07:04 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,9 @@ def profile_page(name):
 	return render_template("profile.html", name=name)
 
 @app.route('/')
-def home_page():
-	return render_template("index.html")
+@app.route('/<user>')
+def home_page(user=None):
+	return render_template("index.html", user=user)
 
 
 
